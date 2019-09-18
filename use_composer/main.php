7bf08494fc47185b98ce079630aab1826c32ac62
Hiroto Yoshidome <kichiru.3161026@gmail.com>
2019-09-19 07:27:27 +0900

    add composer sample

--- use_composer/main.php
+++ use_composer/main.php
@@ -1,3 +1,16 @@
 <?php
-echo "a\n"
+// /usr/local/bin/composer.phar install
+// で関係ライブラリをインストールする
+// composer.phar installで下記のファイルが作成される
+require 'vendor/autoload.php';
+
+// ライブラリ読み込み
+use Carbon\Carbon;
+
+$date = new Carbon('Asia/Tokyo');
+echo $date."\n";
+
+$dt = Carbon::create('Asia/Tokyo');
+echo $dt."\n";
+
 ?>
